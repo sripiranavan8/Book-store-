6add9b690c749289b05d23d8e2e014e56b543d24
Sripiranavan Yogarajah <sripiranavan08@gmail.com>
2021-05-29 20:17:27 +0530

    Befor database setup

--- books/views.py
+++ books/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import json
+
 # Create your views here.
+booksData = open(
+    '/home/sripiranavan/Development/python/django/bookstore-project/books.json').read()
+
+data = json.loads(booksData)
+
+
 def index(request):
-    return HttpResponse('Hello Books App');
+    context = {'books': data}
+    return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+    singleBook = list()
+    for book in data:
+        if book['id'] == id:
+            singleBook = book
+    context = {'book': singleBook}
+    return render(request, 'books/show.html', context)
